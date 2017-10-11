f93879c6a8b1e6edd3a0408300877554d6884f69
Sofía Castillo <sofiaprogramadora@gmail.com>
2017-10-11 10:52:11 -0300

    error user model

--- config/initializers/omniauth.rb
+++
@@ -1,4 +0,0 @@
-# Rails.application.config.middleware.use OmniAuth::Builder do
-#     provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET']
-#     OmniAuth.config.full_host = Rails.env.production? ? 'https://codeclassroom.tk' : 'http://localhost:3000'
-# end
