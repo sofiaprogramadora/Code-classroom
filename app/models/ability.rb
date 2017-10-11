442c8fdd3bc338fd332f8e6789c57e9f3fefc921
Sofía Castillo <sofiaprogramadora@gmail.com>
2017-10-11 10:59:05 -0300

    error ability

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,7 +4,7 @@ class Ability
   def initialize(user)
     # Define abilities for the passed in user here. For example:
     #
-    user ||= User.new(role: 'guest') # guest user (not logged in)
+    user ||= User.new(roles: 'guest') # guest user (not logged in)
     if user.role == "admin"
       can :manage, :all
     elsif user.role == "normal"
