b1df66eee758b51c37408dde2330632c2945134f
Sofía Castillo <sofiaprogramadora@gmail.com>
2017-10-11 10:38:16 -0300

    error assets solved

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -11,7 +11,7 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
-Rails.application.config.assets.precompile += %w( *.jpg *.png *.gif *.ttf *.otf *.woff *.woff2 *.svg *.eot)
+Rails.application.config.assets.precompile += %w( *.jpeg *.jpg *.png *.gif *.ttf *.otf *.woff *.woff2 *.svg *.eot)
 Rails.application.config.assets.paths << Rails.root.join('vendor', 'fonts')
 Rails.application.config.assets.paths << Rails.root.join('vendor', 'js')
 Rails.application.config.assets.paths << Rails.root.join('vendor', 'css')
